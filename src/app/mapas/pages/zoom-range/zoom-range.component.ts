47cec4fbcc7a95a4523392883f8b5aa1653c54ca
neftec99 <neftalitorres@tec.mx>
2021-09-28 09:20:49 -0500

    video : 308

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,9 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl'; /*Con este codigo remplaza esto:  var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js'); */
 
 @Component({
   selector: 'app-zoom-range',
   templateUrl: './zoom-range.component.html',
   styles: [
+    `
+      .map-container{
+        width:100%;
+        height :100%;
+      }
+
+      .row{
+        background-color: white;
+        border-radius:5px;
+        bottom: 50px;
+        left:50px;
+        padding:10px;
+        position: fixed;
+        z-index : 999;
+      }
+    `
   ]
 })
 export class ZoomRangeComponent implements OnInit {
@@ -11,6 +28,12 @@ export class ZoomRangeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    var map = new mapboxgl.Map({
+      container: 'mapa',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-75.921029433568,45.28719674822362],
+      zoom: 18
+      });
   }
 
 }
